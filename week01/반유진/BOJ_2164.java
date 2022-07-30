c0ac5c5d89749fd9970987a2890009cb6bf51c25
dbwls387@naver.com <dbwls387@naver.com>
2022-07-30 20:42:38 +0900

    반유진: BJ 2164 카드2

--- week01/반유진/BOJ_2164.java
+++ week01/반유진/BOJ_2164.java
@@ -17,10 +17,10 @@ public class BOJ_2164 {
 			que.add(i);
 		}
 
-		while (que.size() > 1) {	// 카드가 한장만 남을때까지 반복
-			que.poll();	// 제일 위의 카드 버리기
-			int first = que.poll();	// 제일 위의 카드를 
-			que.add(first);	//제일 아래로 옮기기	
+		while (que.size() > 1) { // 카드가 한장만 남을때까지 반복
+			que.poll(); // 제일 위의 카드 버리기
+			int first = que.poll(); // 제일 위의 카드를
+			que.add(first); // 제일 아래로 옮기기
 		}
 		System.out.println(que.poll());
 	}
