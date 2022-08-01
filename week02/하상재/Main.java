7f7a1d55feb91194321a546550a1ac7cb5da5136
yuseong <dlbia2009@gmail.com>
2022-08-01 15:09:27 +0900

    하상재 : TC 모험가길드

--- week02/하상재/Main.java
+++ week02/하상재/Main.java
@@ -2,6 +2,7 @@ package study;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -17,9 +18,11 @@ public class Main {
 			arr[i] = Integer.parseInt(st.nextToken()); 
 		}
 		
+		Arrays.sort(arr); // Arrays.sort로 정렬해줌 
+		
 		for(int maxFear : arr) {
 			numOfGroup++; // 그룹원 수를 증가 시킴
-			if(numOfGroup>=maxFear) { // 만약 그룹원 수가 빼왔던 공포치 보다 크거나 같다면 수행
+			if(numOfGroup>=maxFear) { // 만약 그룹원 수가 빼왔던 공포치 보다 크거나 같다면 그룹이 성립된거임
 				answer++; // 정답 수 하나 늘려줌
 				numOfGroup = 0; // 그룹원을 0명으로 초기화 해줌
 			}
