6a2c5b2a5870c79c9ed42a57877f97b4eccd18e4
mauv2sky <mauv2sky@gmail.com>
2022-08-09 22:02:40 +0900

    김아현 : HW BJ2563 정사각형 방

---
+++ week03/김아현/BJ2563_정사각형방.java
@@ -0,0 +1,24 @@
+package forStudy.week03;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ2563 {
+
+	static int N;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		
+		N = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<N; i++) {
+			// 흰 도화지 위에 붙여진 검은색 색종이들의 넓이 구하기
+			// 겹쳐진 부분 계산하기 (100*N - 겹쳐진부분의 넓이)
+			
+		}
+	}
+
+}
