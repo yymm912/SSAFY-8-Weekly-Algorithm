2767a2aa6cd067facd404755db57b88157c14cf6
nayu <nyj3230@naver.com>
2022-08-18 22:53:16 +0900

    나유진: BJ 10844 쉬운계단수

---
+++ week04/나유진/BJ_10844_쉬운계단수.java
@@ -0,0 +1,36 @@
+package algorithm_assignments;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class bj_10844_쉬운계단수 {
+	static int N;
+	static long[][] ans = new long[101][10];
+	static long result, mod = 1_000_000_000L;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		N = Integer.parseInt(br.readLine());
+
+		dp();
+
+		for (int i = 0; i <= 9; i++) {
+			result = (result + ans[N][i]) % mod;
+		}
+		System.out.println(result);
+	}
+
+	static void dp() {
+		for (int i = 1; i <= 9; i++) {
+			ans[1][i] = 1;
+		}
+		for (int i = 2; i <= N; i++) {
+			ans[i][0] = ans[i - 1][1];
+			for (int j = 1; j < 9; j++) {
+				ans[i][j] = ((ans[i - 1][j - 1] % mod) + (ans[i - 1][j + 1] % mod));
+			}
+			ans[i][9] = ans[i - 1][8];
+		}
+	}
+}
--- week04/나유진/bj_10844_쉬운계단수.java
+++
@@ -1,7 +0,0 @@
-package BJ;
-
-public class bj_10844_쉬운계단수 {
-	public static void main(String[] args) {
-		
-	}
-}
