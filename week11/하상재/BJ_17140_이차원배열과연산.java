1402e40f171bd7feb60cdcd839b989fefd4c26b9
yuseong <dlbia2009@gmail.com>
2022-10-11 23:29:32 +0900

    하상재 : BJ 17140 이차원배열과연산

---
+++ week11/하상재/BJ_17140_이차원배열과연산.java
@@ -0,0 +1,51 @@
+package study;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_17140_이차원배열과연산 {
+	
+	static int N;
+	static int[][] val;
+	static boolean[][] check;
+	
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		N = Integer.parseInt(br.readLine());
+		int M = Integer.parseInt(br.readLine());
+		
+		val = new int[M][2];
+		check = new boolean[N+1][N+1];
+		
+		for(int i=0; i<M; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int tmp1 = Integer.parseInt(st.nextToken());
+			int tmp2 = Integer.parseInt(st.nextToken());
+			check[tmp2][tmp1] = true;
+		}
+		
+		for(int k=1; k<=N; k++) {
+			for(int i=1; i<=N; i++) {
+				if(k==i) continue;
+				for(int j=1; j<=N; j++) {
+					if(i==j) continue;
+					if(check[i][k] && check[k][j]) check[i][j] = true;
+				}	
+			}
+		}
+		
+		for(int i=1; i<=N; i++) {
+			int cnt = 1;
+			for(int j=1; j<=N; j++) {
+				if(i==j) continue;
+				if(check[i][j] || check[j][i]) cnt++;
+			}
+			sb.append(N-cnt+"\n");
+		}
+		System.out.println(sb);
+	}
+	
+}
