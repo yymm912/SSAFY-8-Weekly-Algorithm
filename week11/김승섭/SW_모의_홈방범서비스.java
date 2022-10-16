c345933a61078982cecafebc59e77e68c33ef330
sub9707 <110171787+sub9707@users.noreply.github.com>
2022-10-16 19:00:07 +0900

    김승섭: SW 모의 홈방범서비스

--- week11/김승섭/SW_모의_홈방범서비스.java
+++ week11/김승섭/SW_모의_홈방범서비스.java
@@ -67,3 +67,96 @@ public class SW_홈방범서비스 {
 	}
 
 }
+
+// BFS 로 풀이
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+ 
+/**
+ * 운영 비용 K K = K * K + (K - 1) * (K - 1) 지점 간 거리 = 원좌표 - (K - 1)
+ */
+public class Solution {
+    static int N, M, map[][], max, hcnt;
+    static boolean visited[][];
+    static Queue<Cell> queue = new ArrayDeque<>();
+ 
+    static class Cell {
+        int y, x;
+ 
+        public Cell(int y, int x) {
+            this.y = y;
+            this.x = x;
+        }
+    }
+ 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+ 
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            map = new int[N][N];
+            hcnt = 0;
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < N; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                    if (map[i][j] == 1)
+                        hcnt++;
+                }
+            }
+            max = Integer.MIN_VALUE;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    visited = new boolean[N][N];
+                    queue.clear();
+                    bfs(i, j);
+                }
+            }
+            System.out.println("#" + tc + " " + max);
+        }
+    }
+ 
+    static int[] dy = { -1, 1, 0, 0 };
+    static int[] dx = { 0, 0, -1, 1 };
+ 
+    static void bfs(int y, int x) {
+        visited[y][x] = true;
+        queue.offer(new Cell(y, x)); // K = 1일 때
+        int K = 1;
+        int size, cnt = 0;
+        Cell cur;
+        while (!queue.isEmpty()) {
+ 
+            size = queue.size();
+            while (--size >= 0) {
+                cur = queue.poll();
+                if (map[cur.y][cur.x] == 1)
+                    cnt++;
+ 
+                for (int d = 0; d < 4; d++) {
+                    int curX = cur.x + dx[d];
+                    int curY = cur.y + dy[d];
+ 
+                    if (curX < 0 || curX >= N || curY < 0 || curY >= N || visited[curY][curX]) continue;
+ 
+                    queue.offer(new Cell(curY, curX));
+                    visited[curY][curX] = true;
+ 
+                }
+            }
+ 
+            if (M * cnt >= (K * K + (K - 1) * (K - 1)))
+                max = Math.max(max, cnt);
+            K++;
+        } 
+    }
+ 
+ 
+}
